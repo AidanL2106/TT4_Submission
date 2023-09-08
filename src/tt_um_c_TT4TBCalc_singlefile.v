9564392171887ada4caad37ae9c303a40a971479
AidanL2106 <144415809+AidanL2106@users.noreply.github.com>
2023-09-08 10:56:13 -0500

    Update tt_um_c_TT4TBCalc_singlefile.v

--- src/tt_um_c_TT4TBCalc_singlefile.v
+++ src/tt_um_c_TT4TBCalc_singlefile.v
@@ -1,12 +1,6 @@
 module tt_um_c_All (
-     input  wire [7:0] ui_in,
-     output wire [7:0] uo_out,
-     input  wire [7:0] uio_in,
-     output wire [7:0] uio_out,
-     output wire [7:0] uio_oe,
-     input  wire       ena,    
-     input  wire       clk,
-     input  wire       rst_n
+     input [7:0] io_in,
+     output [7:0] io_out
 );
 
 wire [1:0] tnet_0 = io_in[7:6]; //input
